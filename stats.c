222159da23bb2cb8b8f59124bb5472e496a4ee28
Anjana Anil <anjanashiny22@gmail.com>
2020-11-16 23:57:26 +0530

    Adding code outline and documentation for C1M1 Assesment

--- stats.c
+++ stats.c
@@ -2,20 +2,22 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Assignment - 1
  *
- * <Add Extended Description Here>
+ * Report analytics on the maximum, minimum, mean, and median of the data set.
+ * In addition, you will need to reorder this data set from large to small.
+ * All statistics should be rounded down to the nearest integer.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Anjana Anil
+ * @date 16/11/20
  *
  */
 
@@ -38,6 +40,45 @@ void main() {
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
+
+	print_array(test,SIZE); // prints array
+	print_statistics(test,SIZE); // prints all statistical information. Calls the function to find median mean minimum and maximum values
+
+
+
 }
 
+unsigned char print_array(unsigned char* A, unsigned int B){ //function to print array
+	
+}
+
+
+unsigned char find_mean(unsigned char* A, unsigned int B){// function to find mean
+	
+}
+
+unsigned char find_minimum(unsigned char* A,unsigned int B){ //function to find minimum
+    
+}
+
+unsigned char find_maximum(unsigned char* A,unsigned int B){ // function to find maximum
+	
+}
+
+
+unsigned char sort_array(unsigned char *A,unsigned int B){// function to sort array
+    
+}
+
+unsigned char find_median(unsigned char* A,unsigned int B){// function to find median
+    
+}
+
+
+unsigned char print_statistics(unsigned char* A,unsigned int B){ // function to print statistics
+    
+}
+
+
 /* Add other Implementation File Code Here */
+
--- stats.h
+++ stats.h
@@ -1,40 +1,77 @@
+
 /******************************************************************************
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Assignment - 1
  *
- * <Add Extended Description Here>
+ * Report analytics on the maximum, minimum, mean, and median of the data set. In addition, you will need to reorder this data set from large to small. All statistics should be rounded down to the nearest integer
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Anjana Anil
+ * @date 16/11/20
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/* Add Your Declarations and Function Comments here */
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief To find the statistics of an array
  *
- * <Add Extended Description Here>
+ *  The mean median maximum minimum are all printed given a pre - defined array.The array is also sorted with sort_array function.
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
+ * @param print_statistics()   A function that prints the statistics of an array including minimum, maximum, mean, and median.
+ * @return An unsigned char result from the function
  *
- * @return <Add Return Informaiton here>
+ * @param print_array() Given an array of data and a length, prints the array to the screen
+ * @return An unsigned char result from the function
+ *
+ * @param find_median() Given an array of data and a length, returns the median value
+ * @return An unsigned char result from the function
+ *
+ * @param find_mean() Given an array of data and a length, returns the mean
+ * @return An unsigned char result from the function
+ *
+ * @param find_maximum()  Given an array of data and a length, returns the maximum
+ * @return An unsigned char result from the function
+ *
+ * @param find_minimum()  Given an array of data and a length, returns the minimum
+ * @return An unsigned char result from the function
+ *
+ * @param sort_array()    Given an array of data and a length, sorts the array from largest to smallest. (The zeroth Element should be the largest value, and the last element (n-1) should be the smallest value. )
+ * @return An unsigned char result from the function
+ *
+ *
+ *
  */
+
+unsigned char print_array(unsigned char*,unsigned int);
+unsigned char find_median(unsigned char* ,unsigned int);
+unsigned char find_mean(unsigned char*,unsigned int);
+unsigned char find_minimum(unsigned char* ,unsigned int);
+unsigned char find_maximum(unsigned char* ,unsigned int);
+unsigned char print_statistics(unsigned char* ,unsigned int );
+unsigned char sort_array(unsigned char* ,unsigned int);
+
+
+
+
+
+
+
+
+
+
 
 
 #endif /* __STATS_H__ */
+
